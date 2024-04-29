4d7f956486750bbde38c0005e4ee6e54e79162a4
ling feng <2809926136@qq.com>
2024-04-29 07:55:31 +0000

    vcx

--- hello_console/lib/hello_console.dart
+++ hello_console/lib/hello_console.dart
@@ -1,9 +1,20 @@
-import 'package:http/http.dart' as http;
+import 'dart:io';
 
-void main() async {
-  var url = Uri.parse('https://jsonplaceholder.typicode.com/posts/1');
-  var response = await http.get(url);
-
-  print('Response status: ${response.statusCode}');
-  print('Response body: ${response.body}');
+void main() {
+  HttpServer.bind(InternetAddress.anyIPv4, 8080).then((server) {
+    print('Server running on localhost:${server.port}');
+    server.listen((HttpRequest request) {
+      // 获取查询参数
+      Map<String, String> queryParams = request.uri.queryParameters;
+      
+      // 提取查询参数中的数据
+      String name = queryParams['name'] ?? 'Unknown';
+      String age = queryParams['age'] ?? 'Unknown';
+      
+      // 构造响应
+      request.response
+        ..write('Hello, $name! Your age is $age.')
+        ..close();
+    });
+  });
 }
